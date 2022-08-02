676a33c4eefe1dbd4046209e71296dc885815cd5
qais-alsgher <qaisalsgher@gmail.com>
2022-08-02 23:14:37 +0300

    mane city

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+
 
 class Main extends React.Component{
     
 render(){
     return (
         <>
-        <img src={this.props.imgUrl} class="img-fluid" alt=""></img>
-        <h5>{this.props.name}</h5>
+        <img src={this.props.imgUrl}  alt=""></img>
+        <h5>{this.props.cityName}</h5>
         <p>lat:{this.props.lat}  lon:{this.props.lon} </p>
          </> 
     )
